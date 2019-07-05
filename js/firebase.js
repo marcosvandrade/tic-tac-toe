e6b4a35b7e5915dc18e1b99c5b33d2bca9e43815
Marcos Vasconcellos de Andrade <dcemvasconcellos@dgp.local>
2019-07-05 10:14:02 -0300

    work5Jul19

---
+++ js/firebase.js
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Document</title>
+</head>
+<body>
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/6.2.4/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#config-web-app -->
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyC0c9FjI1-f4AZzKaFZDoklgUn0J9LDbms",
+    authDomain: "game-9c318.firebaseapp.com",
+    databaseURL: "https://game-9c318.firebaseio.com",
+    projectId: "game-9c318",
+    storageBucket: "",
+    messagingSenderId: "601439684710",
+    appId: "1:601439684710:web:122879522486499f"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+</script>
+</body>
+
+</html>
